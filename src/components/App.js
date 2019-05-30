175e6ab682d511c79e736c9a533092f7c9cb0d93
adamyelen <adam.yelen@gmail.com>
2019-05-30 15:34:46 +0200

    pierwszy push

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = language => {
+        this.setState({ language: language })
+    };
+
+    render() {
+        return (
+            <div className='ui container'>
+                <div>
+                    Select a language
+                    <i className='flag gb uk' onClick={() => this.onLanguageChange('english')} />
+                    <i className='flag pl' onClick={() => this.onLanguageChange('polski')} />
+                    <ColorContext.Provider value='red'>
+                        <LanguageContext.Provider value={this.state.language}>
+                            <UserCreate />
+                        </LanguageContext.Provider>
+                    </ColorContext.Provider>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/Field.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Field extends React.Component {
+    static contextType = LanguageContext;
+
+    render() {
+        const text = this.context === 'english' ? 'Name' : 'Imię';
+
+        return (
+            <div className='ui field'>
+                <label htmlFor="">{text}</label>
+                <input type="text"/>
+            </div>
+        )
+    }
+}
+
+export default Field;
